2ede24ae652fcaec4bc1ffd60c11daa2f72cbc0a
minsk0105 <cminseog7065@gmail.com>
2024-10-11 01:06:49 +0900

    docs : Modularization for home.php

---
+++ CodeFix/Common/footer.php
@@ -0,0 +1,149 @@
+    <!-- 푸터 영역 -->
+    <footer>
+
+        <!-- 푸터 카테고리 -->
+        <div class="footer-top">
+
+            <!-- 문의 방법 -->
+            <div class="inquiry_button">
+                <button type="button">카카오톡 상담</button>
+                <button type="button">입점 및 제휴 문의</button>
+                <button type="button">B2B 대량 구매 문의</button>
+            </div>
+
+            <!-- 편의 안내 -->
+            <div class="category">
+
+                <!-- 고객센터 -->
+                <div class="customer">
+                    <p>고객센터</p>
+                    <span class="call">02-1234-5678</span>
+                    <span>평일 오전 9시 ~ 오후 6시</span>
+                    <span>주말 및 공휴일 휴무</span>
+                </div>
+
+                <!-- 가이드 -->
+                <div class="guide">
+                    <p id="guide_btn">이용안내</p>
+                    <div class="guide_menu">
+                        <a href="#">자주 묻는 질문</a>
+                        <a href="#">취소 & 환불약관</a>
+                        <a href="#">이용약관</a>
+                        <a href="#">개인정보처리방침</a>
+                    </div>
+                    <i class="fa-solid fa-plus" id="plus"></i>
+                    <i class="fa-solid fa-minus" id="minus"></i>
+                </div>
+            </div>
+
+            <!-- 소셜 미디어 아이콘 -->
+            <div class="social_icon">
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-youtube"></i>
+                <i class="fa-brands fa-x-twitter"></i>
+                <i class="fa-brands fa-square-facebook"></i>
+            </div>
+        </div>
+
+        <!-- 회사 정보 -->
+        <div class="footer-bottom">
+            <p>
+                <span class="footer-company_info">주식회사 인테리어티쳐</span>
+                <span class="footer-company_info">대표: 텍스트</span>
+                <span class="footer-company_info">주소: 서울특별시 서초구 방배중앙로 24 4-5층</span>
+                <span class="footer-company_info">사업자등록번호: 000-00-00000</span>
+                <span class="footer-company_info">통신판매업신고번호: 2021-서울서초-3345</span>
+                <span class="footer-company_info">이메일: info@example.com</span><br>
+                <span>&copy;2024 LoremCompany. All Rights Reserved.</span>
+            </p>
+        </div>
+
+    </footer>
+
+    <!-- top 버튼 -->
+    <a href="#" class="top">
+        <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="fill-black-950">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M19.7936 11.5L11.6535 3.35343L12.3609 2.64661L21.7068 12L12.3609 21.3534L11.6536 20.6466L19.7936 12.5L3 12.5V11.5L19.7936 11.5Z" fill="current"></path>
+        </svg>
+    </a>
+
+    <!-- 사이드바 영역 -->
+    <section id="bar_wrap" class="show_wrap">
+
+        <div class="bar_box">
+
+            <!-- 사이드바 닫기 버튼 -->
+            <div class="close_bar">
+                <svg id="remove_bar" width="1em" height="1em" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M9.99165 9.40191L16.0605 3.33301L16.6498 3.92226L10.5809 9.99116L16.6495 16.0597L16.0602 16.649L9.99165 10.5804L3.92307 16.649L3.33381 16.0597L9.40239 9.99116L3.3335 3.92226L3.92275 3.33301L9.99165 9.40191Z" fill="currentColor"></path>
+                </svg>
+            </div>
+
+            <!-- 사이드바 서브메뉴 -->
+            <div class="bar_contents bar_submenu">
+                <p>살펴보기</p>
+                <ul>
+                    <li><a href="#">나만의 가구 만들기</a></li>
+                    <li><a href="#">구매상담 신청하기</a></li>
+                    <li><a href="#">컨설팅 사례</a></li>
+                </ul>
+            </div>
+
+            <div class="bar_contents mypage">
+                <p>마이페이지</p>
+                <ul>
+                    <li>
+                        <a href="#">내 정보</a>
+                        <a href="#">장바구니</a>
+                    </li>
+
+                    <li>
+                        <a href="#">관심 목록</a>
+                        <a href="#">디자인 프로젝트</a>
+                    </li>
+
+                    <li>
+                        <a href="#">구매 내역</a>
+                        <a href="#">나의 쿠폰</a>
+                    </li>
+
+                    <li>
+                        <a href="#">나의 후기</a>
+                        <a href="#">지난 설문</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="bar_contents question">
+                <p>자주 묻는 질문</p>
+                <ul>
+                    <li>
+                        <a href="#">배송관련</a>
+                        <a href="#">취소/교환/반품</a>
+                    </li>
+
+                    <li>
+                        <a href="#">비용/결제/환불</a>
+                        <a href="#">혜택</a>
+                    </li>
+
+                    <li>
+                        <a href="#">인테리어 컨설팅</a>
+                        <a href="#">서비스/일반</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="bar_login">
+                <a href="#">로그인</a>
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- 스크립트 -->
+    <script src="Script/script.js"></script>
+
+</body>
+</html>
